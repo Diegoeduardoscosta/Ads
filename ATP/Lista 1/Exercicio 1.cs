91d66865cdd567f6ee756924d74cf8cb79890e95
Diego Eduardo <diegoeduardoscosta@gmail.com>
2023-03-26 18:51:30 -0300

    Teste de commit

---
+++ ATP/Lista 1/Exercicio 1.cs
@@ -0,0 +1,27 @@
+public static class Atividade2Q1
+{
+    public static void Rodar(){
+        Console.WriteLine("Questão 3");
+
+        //declaração de variáveis
+        double a, b, di, pe, ar;
+
+        //entrada de dados
+        Console.WriteLine("Entre com a base");
+        b = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Entre com a altura");
+        a = double.Parse(Console.ReadLine());
+
+        //Executar os cálculos
+        pe = 2 * (a + b);
+        ar = a * b;
+
+        di = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+        
+        //Saída de dados
+        Console.WriteLine("Resultado do perimetro: " + pe);
+        Console.WriteLine("Resultado do área é: " + ar);
+        Console.WriteLine("Resultado da diagonal é: " + di.ToString("F2"));
+    }
+}
