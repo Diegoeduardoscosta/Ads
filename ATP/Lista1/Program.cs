1baba0c76bcaf853152af6227aa07cde72c4281e
Diego Eduardo <diegoeduardoscosta@gmail.com>
2023-03-26 20:34:48 -0300

    Atualizacao de teste 2

---
+++ ATP/Lista1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System; 
+
+namespace Lista1
+{
+    public class Program
+
+    { 
+         static void Main(string [] args)
+        {
+            Console.WriteLine("Segunda Execução");
+
+        }
+    }
+    
+}
